06a60eeb6ceeab0a89d3677055c5a03bedde3c5f
mjmcandrew <mcandrew713@gmail.com>
2020-09-18 14:58:14 -0500

    Complete and functional

--- Bioinformatics2_Spring2020/Func_EulerianCycle.py
+++ Bioinformatics2_Spring2020/Func_EulerianCycle.py
@@ -1,53 +1,29 @@
 import random
 
-def EulerianCycle(graph, path = None, unexplored = None, current_node = None, target_node = None):
-    print('initializing EulerianCycle with path = {path}, unexplored = {unexplored}, current_node = {current_node}, target_node = {target_node}'.format(path = path, unexplored = unexplored, current_node = current_node, target_node = target_node))
+def EulerianCycle(graph):
     cycle = []
-    if path is None:
-        path = []
-        print('initial path is: ', path)
-    if unexplored is None:
-        unexplored = []
-        for key in graph:
-            unexplored.append(key)
-        print('initial unexplored is: ', unexplored)
-    if current_node is None:
-        current_node = random.choice(list(graph))
-        target_node = current_node
-        print('initial starting/target node is: ', current_node)
-    print('adding current node to path...')
+    path = []
+    current_node = random.choice(list(graph))
     path.append(current_node)
-    print('new path is: ', path)
-    while unexplored:
-        print('finding neighbors...')
+    while path:
         if graph[current_node]:
-            if len(graph[current_node]) == 1:
-                print('this node only has one adjacency. removing from unexplored')
-                unexplored.remove(current_node)
-                print('new unexplored is: ', unexplored)
             neighbor = graph[current_node].pop()
-            print('neighbor is ', neighbor)
-            print('graph is ', graph)
             current_node = neighbor
+            path.append(current_node)
         else:
-            #need to fix the logic here so that it backtracks or something
-            print('dead end. reinitializing...')
-            #break
-            cycle = None
-            unexplored = None
-            current_node = None
-            target_node = None
-        return EulerianCycle(graph, path, unexplored, current_node, target_node)
-    return path, cycle
+            cycle.append(path.pop())
+            if path:
+                current_node = path[-1]
+    return cycle[::-1]
 
-if __name__ == "__main__":
-    import sys
-    Input = sys.stdin.readlines()
-    Input_graph = dict(line.strip().split(' -> ') for line in Input)
+
+with open("EulerianCycle.txt") as f:
+    Input_graph = dict(line.strip().split(' -> ') for line in f)
     graph = {}
     for key, value in Input_graph.items():
-        graph[int(key)] = [int(val) for val in value.split(',')]
+        graph[key] = [val for val in value.split(',')]
 
-print(graph)
+
+
 result = EulerianCycle(graph)
-#print('->'.join([str(val) for val in result]))
+print('->'.join([str(val) for val in result]))
